fc275a3759db9a0ae74145503d217bdbdae19235
JYLI-at-eada <lijiayue620@gmail.com>
2025-06-18 23:25:12 +0200

    temporarily comment out mlflow uri to unblock CD

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ def main():
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    mlflow.set_tracking_uri("http://127.0.0.1:8081")
-    mlflow.set_experiment("Delivery Time Model Comparison")
+    #mlflow.set_tracking_uri("http://127.0.0.1:8081")
+    #mlflow.set_experiment("Delivery Time Model Comparison")
 
     models = [
         ("Linear", train_lr(X_train, y_train)),
